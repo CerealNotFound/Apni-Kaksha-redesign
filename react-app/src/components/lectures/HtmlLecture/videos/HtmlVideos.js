fdce7f207a477e467bc6d64374259c148f76306b
Adarsh Pandey <72287529+CerealNotFound@users.noreply.github.com>
2021-11-10 12:07:03 +0000

    Debugging

--- react-app/src/components/lectures/HtmlLecture/videos/HtmlVideos.js
+++ react-app/src/components/lectures/HtmlLecture/videos/HtmlVideos.js
@@ -3,27 +3,27 @@ import ReactDOM from "react-dom";
 
 const Video1 = () => {
   return ReactDOM.createPortal(
-    <div className="modal-iframe">
+    <div className="modal-iframe-wrapper">
       <div className="cross-modal"></div>
-      <iframe width="640" height="360" src="https://www.youtube-nocookie.com/embed/Rek0NWPCNOc?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe className="modal-iframe" width="640" height="360" src="https://www.youtube-nocookie.com/embed/Rek0NWPCNOc?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>, document.getElementById("portal")
     )
 }
 
 const Video2 = () => {
   return ReactDOM.createPortal(
-    <div className="modal-iframe">
+    <div className="modal-iframe-wrapper">
       <div className="cross-modal"></div>
-      <iframe width="640" height="360" src="https://www.youtube-nocookie.com/embed/2QR11oDukn4?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe className="modal-iframe" width="640" height="360" src="https://www.youtube-nocookie.com/embed/2QR11oDukn4?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>, document.getElementById("portal")
   )
 }
 
 const Video3 = () => {
   return ReactDOM.createPortal(
-    <div className="modal-iframe">
+    <div className="modal-iframe-wrapper">
       <div className="cross-modal"></div>
-      <iframe width="640" height="360" src="https://www.youtube-nocookie.com/embed/jRAZlTEZi9I?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe className="modal-iframe" width="640" height="360" src="https://www.youtube-nocookie.com/embed/jRAZlTEZi9I?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>, document.getElementById("portal")
   )
 }
--- react-app/src/components/lectures/ProjectLecture/videos/ProjectVideos.js
+++ react-app/src/components/lectures/ProjectLecture/videos/ProjectVideos.js
@@ -3,18 +3,18 @@ import ReactDOM from "react-dom";
 
 const Video1 = () => {
   return ReactDOM.createPortal(
-    <div className="modal-iframe">
+    <div className="modal-iframe-wrapper">
       <div className="cross-modal"></div>
-      <iframe width="640" height="360" src="https://www.youtube-nocookie.com/embed/IZa8pbR8CWs?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe className="modal-iframe" width="640" height="360" src="https://www.youtube-nocookie.com/embed/IZa8pbR8CWs?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>, document.getElementById("portal")
     )
 }
 
 const Video2 = () => {
   return ReactDOM.createPortal(
-    <div className="modal-iframe">
+    <div className="modal-iframe-wrapper">
       <div className="cross-modal"></div>
-      <iframe width="640" height="360" src="https://www.youtube-nocookie.com/embed/ywK_wgQwKJY?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe className="modal-iframe" width="640" height="360" src="https://www.youtube-nocookie.com/embed/ywK_wgQwKJY?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>, document.getElementById("portal")
   )
 }
